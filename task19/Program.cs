4ccc4fe4686a8c624c12100704ee9c579286fc78
¨Galina¨ <galina.jounda@gmail.com>
2022-10-02 12:09:08 +0200

    home work 3

---
+++ task19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task19
+{
+    class Program
+    {
+        //Задача 19. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+        //14212 -> нет
+        //12821 -> да
+        //23432 -> да
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите пятизначное число");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num < 10000 || num > 99999)
+            {
+                Console.WriteLine("Число не пятизначное");
+            }
+
+            Console.WriteLine(num / 10000 == num % 10 && num % 10000 / 1000 == num % 100 / 10 ? "да" : "нет");
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ task23/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task23
+{
+    class Program
+    {
+        //Задача 23. Напишите программу, которая принимает на вход число(N) и выдаёт таблицу кубов чисел от 1 до N.
+        //3 -> 1, 8, 27
+        //5 -> 1, 8, 27, 64, 125
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            TableCube(num);
+            Console.ReadLine();
+        }
+
+        static void TableCube(int arg)
+        {
+            int i = 1;
+            if (arg > 0)
+            {
+                while (i <= arg)
+                {
+                    Console.WriteLine($"{i} {Math.Pow(i, 3)}");
+                    i++;
+                }
+            }
+        }
+    }
+}
