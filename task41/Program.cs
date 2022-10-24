abb908745e54222a491cc5c01ceca152e8f9c21c
¨Galina¨ <galina.jounda@gmail.com>
2022-10-24 18:15:08 +0200

    home work 6

---
+++ task41/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task41
+{
+    class Program
+    {
+        //Задача 41: Пользователь вводит с клавиатуры M чисел.Посчитайте, сколько чисел больше 0 ввёл пользователь.
+        //0, 7, 8, -2, -2 -> 2
+        //-1, -7, 567, 89, 223-> 3
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите несколько чисел через запятую");
+            var numbers = GetNumbers();
+            ShowResult(numbers);
+            Console.ReadLine();
+        }
+
+        static int[] GetNumbers()
+        {
+            var value = Console.ReadLine();
+            var strings = value.Split(new string[] { ", ", "," }, StringSplitOptions.RemoveEmptyEntries);
+            var numbers = new int[strings.Length];
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(strings[i]);
+            }
+            return numbers;
+        }
+
+        static void ShowResult(int[] numbers)
+        {
+            var result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i] > 0 ? 1 : 0;
+            }
+
+            Console.WriteLine($"{string.Join(", ", numbers)} -> {result}");
+        }
+    }
+}
---
+++ task43/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task43
+{
+    class Program
+    {
+        //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+        //заданных уравнениями y = k1* x + b1, y = k2* x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+        //b1 = 2, k1 = 5, b2 = 4, k2 = 9-> (-0, 5; -0,5)
+        static void Main(string[] args)
+        {
+            var k1 = GetNumber("k1");
+            var b1 = GetNumber("b1");
+            var k2 = GetNumber("k2");
+            var b2 = GetNumber("b2");
+
+            ShowIntersection(k1, b1, k2, b2);
+            Console.ReadLine();
+        }
+
+        static int GetNumber(string input)
+        {
+            Console.WriteLine($"Введите переменную {input}");
+            var value = Console.ReadLine();
+            return Convert.ToInt32(value);
+        }
+
+        static void ShowIntersection(double k1, double b1, double k2, double b2)
+        {
+            var x = (b2 - b1) / (k1 - k2);
+            var y = k1 * x + b1;
+            Console.WriteLine($"({x}, {y})");
+        }
+    }
+}
